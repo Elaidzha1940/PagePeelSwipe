4e47487ab040667e59214239ef4e8dc2e99f86a9
Elaidzha1940 <eli080293@gmail.com>
2024-04-12 17:41:49 +0300

    PagePeelSwipe

---
+++ PagePeelSwipe/ContentView.swift
@@ -0,0 +1,22 @@
+//  /*
+//
+//  Project: PagePeelSwipe
+//  File: ContentView.swift
+//  Created by: Elaidzha Shchukin
+//  Date: 12.04.2024
+//
+//  */
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+          
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PagePeelSwipe/PagePeelSwipeApp.swift
@@ -0,0 +1,19 @@
+//  /*
+//
+//  Project: PagePeelSwipe
+//  File: PagePeelSwipeApp.swift
+//  Created by: Elaidzha Shchukin
+//  Date: 12.04.2024
+//
+//  */
+
+import SwiftUI
+
+@main
+struct PagePeelSwipeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
